6ffb3003a8fb3821ca635ac4fa7d0e22e67bd1ec
Kimmo Korpelin <kimmo.korpelin@iceye.fi>
2023-04-21 18:10:36 +0300

    add clangd, copilot cmp sources (nvim)

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -7,14 +7,24 @@ use {
   requires = { 'kyazdani42/nvim-web-devicons', opt = true }
 }
 use 'nvim-lua/plenary.nvim'
-use 'matbme/JABS.nvim'
+use 'neovim/nvim-lspconfig'
+use 'hrsh7th/vim-vsnip'
+use 'hrsh7th/nvim-cmp'
+use 'hrsh7th/cmp-nvim-lsp'
 use 'nvim-telescope/telescope.nvim'
 use 'nvim-tree/nvim-tree.lua'
-use 'willthbill/opener.nvim'
 use 'ryanoasis/vim-devicons'
-use 'ntpeters/vim-better-whitespace'
-use 'github/copilot.vim'
+--use 'ntpeters/vim-better-whitespace'
 use 'airblade/vim-gitgutter'
+-- use 'github/copilot.vim'
+use 'zbirenbaum/copilot.lua'
+use {
+  'zbirenbaum/copilot-cmp',
+  after = { 'copilot.lua' },
+  config = function ()
+    require("copilot_cmp").setup()
+  end
+}
 use { "catppuccin/nvim", as = "catppuccin" }
 
 end)
