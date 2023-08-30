01afe1c7a40552834c94bb779c9a15f1c57fa1a3
Kimmo Korpelin <kimmo.korpelin@iceye.fi>
2023-08-30 19:31:16 +0300

    fast keybind for lsp refs (nvim)

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -86,6 +86,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     local opts = { buffer = ev.buf }
     vim.keymap.set('n', 'gd', vim.lsp.buf.declaration, opts)
     vim.keymap.set('n', 'gD', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
     vim.keymap.set('n', '<space>td', vim.lsp.buf.declaration, opts)
     vim.keymap.set('n', '<space>tD', vim.lsp.buf.definition, opts)
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
