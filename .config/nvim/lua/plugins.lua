95ee08453c7760b07f9e2e9cad45750cc126f0f4
Kimmo Korpelin <kimmo.korpelin@iceye.fi>
2023-08-31 15:26:43 +0300

    add ctrl-click for lsp def (nvim)

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -84,6 +84,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
   callback = function(ev)
     vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
     local opts = { buffer = ev.buf }
+    vim.keymap.set('n', '<C-LeftMouse>', vim.lsp.buf.declaration, opts)
     vim.keymap.set('n', 'gd', vim.lsp.buf.declaration, opts)
     vim.keymap.set('n', 'gD', vim.lsp.buf.definition, opts)
     vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
